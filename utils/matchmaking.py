686297b3501bb544625c4510d492ebe944c2cbb1
Chase McDonald <chasecmcdonald@gmail.com>
2025-05-27 15:35:00 +0200

    add compatibility for new stack in matchmaker

--- utils/matchmaking.py
+++ utils/matchmaking.py
@@ -22,7 +22,8 @@ class Matchmaker:
         self, agent_id: str, episode: EpisodeType, **kwargs
     ) -> str:
         """Policy mapping function that retrieves from the current matchup"""
-        if self.current_matchups.get(episode.env_id) is None:
+        id_ = episode.env_id if hasattr(episode, "env_id") else episode.id_
+        if self.current_matchups.get(id_) is None:
             # Sample a matchup
             sampled_matchup = np.random.choice(self.matchups, p=self.probs)
 
@@ -31,12 +32,12 @@ class Matchmaker:
             p1, p2 = np.random.choice(policies, size=2, replace=False)
 
             # Set this as the current episodes mapping
-            self.current_matchups[episode.env_id]["p1"] = p1
-            self.current_matchups[episode.env_id]["p2"] = p2
+            self.current_matchups[id_]["p1"] = p1
+            self.current_matchups[id_]["p2"] = p2
 
-        pid = self.current_matchups[episode.env_id].pop(agent_id)
+        pid = self.current_matchups[id_].pop(agent_id)
 
-        if not self.current_matchups[episode.env_id]:
-            del self.current_matchups[episode.env_id]
+        if not self.current_matchups[id_]:
+            del self.current_matchups[id_]
 
         return pid
